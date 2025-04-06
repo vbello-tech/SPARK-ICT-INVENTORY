26d61b14fd5d93b407b48a8ef0ace56dca4ff688
vbello <vbello-tech@gmail.com>
2025-04-06 22:46:55 +0100

    fix production issues

--- inventory/models.py
+++ inventory/models.py
@@ -19,23 +19,23 @@ for item in category_list:
     product_cat_choice.append(item)
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=300)
-    color = models.CharField(max_length=300)
-    type = models.CharField(max_length=300, choices=product_cat_choice, blank=True, null=True)
-    imei = models.IntegerField(blank=True, null=True)
-    sold = models.BooleanField(default=False)
-    checked_in = models.BooleanField(default=False)
-    spec = models.CharField(max_length=300)
-    details = models.CharField(max_length=300, blank=True, null=True)
-    date_sold = models.DateField(blank=True, null=True)
-    checked_in_date = models.DateField(blank=True, null=True)
-    seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
-    buyer = models.CharField(max_length=300, blank=True, null=True)
-    buyer_phone = PhoneNumberField(blank=True, null=True)
-
-    def get_quantity(self):
-        return self.quantity
-
-    def __str__(self):
-        return self.name
+# class Product(models.Model):
+#     name = models.CharField(max_length=300)
+#     color = models.CharField(max_length=300)
+#     type = models.CharField(max_length=300, choices=product_cat_choice, blank=True, null=True)
+#     imei = models.IntegerField(blank=True, null=True)
+#     sold = models.BooleanField(default=False)
+#     checked_in = models.BooleanField(default=False)
+#     spec = models.CharField(max_length=300)
+#     details = models.CharField(max_length=300, blank=True, null=True)
+#     date_sold = models.DateField(blank=True, null=True)
+#     checked_in_date = models.DateField(blank=True, null=True)
+#     seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+#     buyer = models.CharField(max_length=300, blank=True, null=True)
+#     buyer_phone = PhoneNumberField(blank=True, null=True)
+#
+#     def get_quantity(self):
+#         return self.quantity
+#
+#     def __str__(self):
+#         return self.name
